62582b00ba0933d7326dfff7e34a2bbdb9e1e8a8
ginny100 <ginnydang0728@gmail.com>
2021-11-23 17:31:08 -0600

    Projects updated

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -242,7 +242,6 @@ $( document ).ready(function() {
 
           break;
         }
-
       }
 
       // A CSS selector is the first part of a CSS Rule. It is a pattern of elements and other terms that tell the browser which HTML elements should be selected to have the CSS property values inside the rule applied to them.
@@ -252,7 +251,6 @@ $( document ).ready(function() {
     $('.slider').animate({ opacity : 1 }, 400);
 
     });
-
   }
 
   function transitionLabels() {
