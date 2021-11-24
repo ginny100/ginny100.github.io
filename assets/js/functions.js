4346f286d4f0b5fe3dbf017306850b3bce755fc7
ginny100 <ginnydang0728@gmail.com>
2021-11-23 18:53:16 -0600

    Projects updated

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -5,7 +5,7 @@ $( document ).ready(function() {
   // DOMMouseScroll included for firefox support
   var canScroll = true,
       scrollController = null;
-  $(this).on('mousewheel DOMMouseScroll', function(e){
+  $(this).on('mousewheel DOMMouseScroll', function(e) {
 
     if (!($('.outer-nav').hasClass('is-vis'))) {
 
@@ -16,7 +16,7 @@ $( document ).ready(function() {
       if (delta > 50 && canScroll) {
         canScroll = false;
         clearTimeout(scrollController);
-        scrollController = setTimeout(function(){
+        scrollController = setTimeout(function() {
           canScroll = true;
         }, 800);
         updateHelper(1);
@@ -24,7 +24,7 @@ $( document ).ready(function() {
       else if (delta < -50 && canScroll) {
         canScroll = false;
         clearTimeout(scrollController);
-        scrollController = setTimeout(function(){
+        scrollController = setTimeout(function() {
           canScroll = true;
         }, 800);
         updateHelper(-1);
@@ -34,7 +34,7 @@ $( document ).ready(function() {
 
   });
 
-  $('.side-nav li, .outer-nav li').click(function(){
+  $('.side-nav li, .outer-nav li').click(function() {
 
     if (!($(this).hasClass('is-active'))) {
 
@@ -51,7 +51,7 @@ $( document ).ready(function() {
 
   });
 
-  $('.cta').click(function(){
+  $('.cta').click(function() {
 
     var curActive = $('.side-nav').find('.is-active'),
         curPos = $('.side-nav').children().index(curActive),
@@ -178,84 +178,84 @@ $( document ).ready(function() {
   function workSlider() {
 
     $('.slider--prev, .slider--next').click(function() {
+      alert('clicked')
 
       $('.slider').animate({ opacity : 0 }, 400);
 
-      setTimeout(function(){
-      
-      let direction = $(this).hasClass('slider--next') ? 1 : -1 // 1 => right, -1 => left
-      
-      /* there are n elements: 0 -> 1 -> 2 -> ... -> n - 2 -> n - 1
-      - n = number of projects = $('.l-section.section .work .work--lockup .slider .slider--item')
-      */
-
-      // value1 if some_conditions else value2 => Python
-      // some_conditions ? value1 : value2 => Javascript
-
-      let allProjects = $('.l-section.section .work .work--lockup .slider .slider--item');
-      let numProjects = allProjects.length;
-
-      // finding the index of the current center element
-      for(let i = 0; i < numProjects; ++i){
-        // allProjects[i] = current project 
+      // setTimeout(function(){
         
-        // if current project is the center
-        if(allProjects.eq(i).hasClass('slider--item-center')){
+      //   let direction = $(this).hasClass('slider--next') ? 1 : -1 // 1 => right, -1 => left
+        
+      //   /* there are n elements: 0 -> 1 -> 2 -> ... -> n - 2 -> n - 1
+      //   - n = number of projects = $('.l-section.section .work .work--lockup .slider .slider--item')
+      //   */
 
-          // Find old center, left, right
-          let oldCenterId = i;
-          let oldLeftId = i == 0 ? numProjects - 1 : i - 1;
-          let oldRightId = i == numProjects - 1 ? 0 : i + 1;
+      //   // value1 if some_conditions else value2 => Python
+      //   // some_conditions ? value1 : value2 => Javascript
 
-          console.log(oldCenterId, oldLeftId, oldRightId);
+      //   let allProjects = $('.l-section.section .work .work--lockup .slider .slider--item');
+      //   let numProjects = allProjects.length;
+
+      //   // finding the index of the current center element
+      //   for(let i = 0; i < numProjects; ++i){
+      //     // allProjects[i] = current project 
           
-          // Remove old center, left, right
-          allProjects.eq(oldCenterId).removeClass('slider--item-center');
-          allProjects.eq(oldLeftId).removeClass('slider--item-left');
-          allProjects.eq(oldRightId).removeClass('slider--item-right');
+      //     // if current project is the center
+      //     if(allProjects.eq(i).hasClass('slider--item-center')){
 
-          allProjects.addClass('slider--item-hidden');
+      //       // Find old center, left, right
+      //       let oldCenterId = i;
+      //       let oldLeftId = i == 0 ? numProjects - 1 : i - 1;
+      //       let oldRightId = i == numProjects - 1 ? 0 : i + 1;
 
-          // Everything is now hidden
-          
-          // Find new center
-          let newCenterId = oldCenterId + direction;
-          if (newCenterId == -1){ newCenterId = numProjects - 1; }
-          else if (newCenterId == numProjects) { newCenterId = 0; }
-          
-          // Find new left
-          let newLeftId = oldLeftId + direction;
-          if (newLeftId == -1){ newLeftId = numProjects - 1; }
-          else if (newLeftId == numProjects) { newLeftId = 0; }
+      //       console.log(oldCenterId, oldLeftId, oldRightId);
+            
+      //       // Remove old center, left, right
+      //       allProjects.eq(oldCenterId).removeClass('slider--item-center');
+      //       allProjects.eq(oldLeftId).removeClass('slider--item-left');
+      //       allProjects.eq(oldRightId).removeClass('slider--item-right');
 
-          // Find new right
-          let newRightId = oldRightId + direction;
-          if (newRightId == -1){ newRightId = numProjects - 1; }
-          else if (newRightId == numProjects) { newRightId = 0; }
+      //       allProjects.addClass('slider--item-hidden');
 
-          console.log(newCenterId, newLeftId, newRightId);
-          
-          // Add new center, left, right
-          allProjects.eq(newCenterId).removeClass('slider--item-hidden').addClass('slider--item-center');
-          allProjects.eq(newLeftId).removeClass('slider--item-hidden').addClass('slider--item-left');
-          allProjects.eq(newRightId).removeClass('slider--item-hidden').addClass('slider--item-right');
+      //       // Everything is now hidden
+            
+      //       // Find new center
+      //       let newCenterId = oldCenterId + direction;
+      //       if (newCenterId == -1){ newCenterId = numProjects - 1; }
+      //       else if (newCenterId == numProjects) { newCenterId = 0; }
+            
+      //       // Find new left
+      //       let newLeftId = oldLeftId + direction;
+      //       if (newLeftId == -1){ newLeftId = numProjects - 1; }
+      //       else if (newLeftId == numProjects) { newLeftId = 0; }
 
-          break;
-        }
-      }
+      //       // Find new right
+      //       let newRightId = oldRightId + direction;
+      //       if (newRightId == -1){ newRightId = numProjects - 1; }
+      //       else if (newRightId == numProjects) { newRightId = 0; }
 
-      // A CSS selector is the first part of a CSS Rule. It is a pattern of elements and other terms that tell the browser which HTML elements should be selected to have the CSS property values inside the rule applied to them.
+      //       console.log(newCenterId, newLeftId, newRightId);
+            
+      //       // Add new center, left, right
+      //       allProjects.eq(newCenterId).removeClass('slider--item-hidden').addClass('slider--item-center');
+      //       allProjects.eq(newLeftId).removeClass('slider--item-hidden').addClass('slider--item-left');
+      //       allProjects.eq(newRightId).removeClass('slider--item-hidden').addClass('slider--item-right');
 
-    }, 400);
+      //       break;
+      //     }
+      //   }
 
-    $('.slider').animate({ opacity : 1 }, 400);
+      //   // A CSS selector is the first part of a CSS Rule. It is a pattern of elements and other terms that tell the browser which HTML elements should be selected to have the CSS property values inside the rule applied to them.
+
+      // }, 400);
+      $('.slider').animate({ opacity : 1 }, 400);
 
     });
   }
 
   function transitionLabels() {
 
-    $('.work-request--information input').focusout(function(){
+    $('.work-request--information input').focusout(function() {
 
       var textVal = $(this).val();
 
